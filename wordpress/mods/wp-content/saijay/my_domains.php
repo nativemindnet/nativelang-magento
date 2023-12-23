3002b09ceedd57a453804192b7680ef40125f35f
Your Name <you@example.com>
2023-12-23 16:51:51 +0700

    no comments

---
+++ wordpress/mods/wp-content/saijay/my_domains.php
@@ -0,0 +1,17 @@
+<?php
+// Configure your additional domains here. Some examples:
+
+cybo_add_extra_domain( 'blog.saijay.com', 1 );
+cybo_add_extra_domain( 'saijay.com', 1 );
+
+cybo_add_extra_domain( 'hi.saijay.com', 1 );
+cybo_add_extra_domain( 'ru.saijay.com', 1 );
+cybo_add_extra_domain( 'th.saijay.com', 1 );
+cybo_add_extra_domain( 'cn.saijay.com', 1 );
+cybo_add_extra_domain( 'hi.saijay.com', 1 );
+
+cybo_add_extra_domain( 'saijay.ru', 1 );
+cybo_add_extra_domain( 'saijay.us', 1 );
+cybo_add_extra_domain( 'saijay.net', 1 );
+cybo_add_extra_domain( 'saijay.in', 1 );
+
